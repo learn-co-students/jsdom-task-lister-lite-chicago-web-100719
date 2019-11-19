b28f0d3521caeda9cfeba2357e1fc706a0b6b05a
Nathan Alex <nate745@gmail.com>
2019-11-19 14:59:26 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const input = document.getElementById('create-task-form')
+  const list = document.getElementById('tasks')
+
+  input.addEventListener('submit', createNewTask)
+
 });
+
+function createNewTask() {
+  event.preventDefault();
+  let task = document.getElementById('new-task-description')
+  let li = document.createElement('li')
+  li.innerText = task.value
+  li.innerHTML = `${task.value}`+ `<button data-description =${task.value} onclick= "destroyTask()" >X</button>` 
+  list.appendChild(li)
+
+  event.target.reset();
+
+}
+
+
+function destroyTask() {
+
+let li = document.querySelector('li')
+li.remove()
+
+}
