9a09ed1103e832f0e5c85d68010a82f22f0aa127
bosmanpa <bsmn.ptrck@gmail.com>
2019-11-19 15:47:40 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+const form = document.getElementById("create-task-form")
+const list = document.getElementById("list");
+  
+function addToDoItem(){
+  event.preventDefault()
+  const newitem = document.getElementById("new-task-description")
+  let task = document.createElement('li');
+  let button = document.createElement('button')
+  task.innerHTML = newitem.value
+  button.innerHTML = "X"
+  list.appendChild(task)
+  task.appendChild(button)
+  button.addEventListener("click", removeToDoItem)
+  newitem.value = ""
+}
+
+
+function removeToDoItem(){
+  event.target.parentElement.remove()
+}
+
+
+form.addEventListener("submit", addToDoItem)
+
+
+
+
+
 });
